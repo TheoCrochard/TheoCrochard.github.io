318498d8dd9d5f75ecb575ceabf6a5b1c87e5593
Théo Crochard <to.crochard@gmail.com>
2025-12-01 14:07:42 +0100

    maj

--- portfolio 2K25/script.js
+++ portfolio 2K25/script.js
@@ -1,16 +1,16 @@
-    function createRain() {
-  const rain = document.querySelector('.rain');
+// function createRain() {
+//   const rain = document.querySelector('.rain');
 
-  for (let i = 0; i < 150; i++) {
-    const drop = document.createElement('div');
-    drop.classList.add('drop');
+//   for (let i = 0; i < 150; i++) {
+//     const drop = document.createElement('div');
+//     drop.classList.add('drop');
 
-    drop.style.left = Math.random() * 100 + "vw";
-    drop.style.animationDuration = 2 + Math.random() * 1.5 + "s";
-    drop.style.animationDelay = Math.random() * 5 + "s";
+//     drop.style.left = Math.random() * 100 + "vw";
+//     drop.style.animationDuration = 2 + Math.random() * 1.5 + "s";
+//     drop.style.animationDelay = Math.random() * 5 + "s";
 
-    rain.appendChild(drop);
-  }
-}
+//     rain.appendChild(drop);
+//   }
+// }
 
-createRain();
+// createRain();
